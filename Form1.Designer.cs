9be99979d9404385ce1004c4550a68bbe782280a
김현섭 <>
2026-03-19 12:32:06 +0900

    과제1 코딩과 테스트 완료 -텍스트박스에 문자를 입력하고 전송버튼을 누르면 리스트박스에 입력한 문자 출력 -텍스트박스를 통해 문자를 전송하면 텍스트박스를 초기화 -텍스트박스를 클릭하면 "문자를 입력하세요"가 사라지고 입력창으로 전환

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            lstMessages = new ListBox();
+            txtboxMessage = new TextBox();
+            btnSend = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("한컴 말랑말랑 Regular", 36F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label1.ForeColor = SystemColors.Highlight;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(387, 62);
+            label1.TabIndex = 0;
+            label1.Text = "Echo Messenger";
+            // 
+            // lstMessages
+            // 
+            lstMessages.Font = new Font("한컴 말랑말랑 Regular", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lstMessages.FormattingEnabled = true;
+            lstMessages.ItemHeight = 21;
+            lstMessages.Location = new Point(22, 74);
+            lstMessages.Name = "lstMessages";
+            lstMessages.Size = new Size(750, 256);
+            lstMessages.TabIndex = 1;
+            // 
+            // txtboxMessage
+            // 
+            txtboxMessage.Font = new Font("한컴 말랑말랑 Regular", 14.2499981F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtboxMessage.Location = new Point(22, 360);
+            txtboxMessage.Name = "txtboxMessage";
+            txtboxMessage.Size = new Size(650, 32);
+            txtboxMessage.TabIndex = 2;
+            txtboxMessage.Text = "문자를 입력하세요";
+            txtboxMessage.Click += txtboxMessage_Click;
+            txtboxMessage.TextChanged += txtboxMessage_TextChanged;
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = SystemColors.GradientInactiveCaption;
+            btnSend.Font = new Font("한컴 말랑말랑 Regular", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnSend.Location = new Point(681, 360);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(91, 32);
+            btnSend.TabIndex = 3;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = false;
+            btnSend.Click += btnSend_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 128);
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSend);
+            Controls.Add(txtboxMessage);
+            Controls.Add(lstMessages);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ListBox lstMessages;
+        private TextBox txtboxMessage;
+        private Button btnSend;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,27 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string typed_msg = txtboxMessage.Text;
+            lstMessages.Items.Add(typed_msg);
+            txtboxMessage.Clear();
+        }
+
+        private void txtboxMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtboxMessage_Click(object sender, EventArgs e)
+        {
+            txtboxMessage.Clear();
+        }
     }
 }
